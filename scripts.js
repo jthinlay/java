870945bc3e9cbae9b8e8140b0bb7e97c63fc2d55
jthinlay <jthinlay@hotmail.com>
2020-08-10 12:15:13 -0700

    added gitignore file and modifired funcion names with self-explanatory naming convention

--- scripts.js
+++ scripts.js
@@ -5,25 +5,21 @@ const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
 const loader = document.getElementById('loader');
 
-//Show loader 
-    function loading(){
+    function showLoadSpinner(){
         loader.hidden = false;
         quoteContainer.hidden = true;
     }
 
-// Hide loader 
-   function complete(){
+   function removeLoadSpinner(){
        if(!loader.hidden){
            quoteContainer.hidden = false;
            loader.hidden = true;
        } 
    }
 
-// Get Quote From API
-async function getQuote() {
+async function getQuoteFromAPI() {
 
-    //starts loader
-    loading();
+    showLoadSpinner();
 
     const proxyUrl = 'https://secret-ocean-38471.herokuapp.com/';
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
@@ -45,25 +41,22 @@ async function getQuote() {
         }
         quoteText.innerText = data.quoteText;
  
-        //stop loader, show quote
-        complete();
+    removeLoadSpinner();
 
     }catch(error) {
-        getQuote();
+        getQuoteFromAPI();
         console.log("Whoops, no quote", error);
     }
 }
 
-// Tweet Quote
 function tweetQuote(){
     const quote = quoteText.innerText;
     const author = authorText.innerText;
     const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
     window.open(twitterUrl, '_blank')
-}
+}                        
 //Event Listeners
-    newQuoteBtn.addEventListener('click', getQuote);
+    newQuoteBtn.addEventListener('click', getQuoteFromAPI);
     twitterBtn.addEventListener('click', tweetQuote)
 
-// Load quote
-getQuote()
+getQuoteFromAPI()
